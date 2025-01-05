5f90ef27f40992525714f3bf48c61c0daabcd675
omkar-nath <omkarnath8039@gmail.com>
2025-01-05 20:47:27 +0530

    added two themes

---
+++ lua/colors/mossy.lua
@@ -0,0 +1,13 @@
+local palette = {
+
+	primaryBackground = "#001b00",
+	keyword = "#d9534f",
+	variable = "#83d1c6",
+	numbers = "#a5acb2",
+	classes = "#f1c27d",
+	functions = "#2e8b57",
+	strings = "#ffd700",
+	operators = "#d8a700",
+	comment = "#7f848e",
+}
+return palette
---
+++ lua/colors/vivid.lua
@@ -0,0 +1,13 @@
+local palette = {
+	primaryBackground = "#222222",
+	keyword = "#ec0091",
+	variable = "#ffb74d",
+	numbers = "#4caf50",
+	classes = "#b177e5",
+	functions = "#76e3e5",
+	strings = "#98c379",
+	operators = "#ff91a4",
+	comment = "#7f848e",
+}
+
+return palette
